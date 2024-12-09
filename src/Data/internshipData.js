b589c340b93af5a32ee1c22c51693982e94197d5
saurabh <saurabhshindework@gmail.com>
2024-12-10 02:27:40 +0530

    Add vercel analytics in project

--- src/Data/internshipData.js
+++ src/Data/internshipData.js
@@ -1,31 +1,64 @@
 const internshipData = [
-    {
-      title: "Software Developer Intern",
-      company: "Incronix Technology Pvt Ltd",
-      location: "Aundh, Pune",
-      duration: "July 2024 - Pursuing",
-      responsibilities: [
-        "Optimized React frontend for performance, improving page load times, enhancing website functionality and user experience.",
-        "Currently Working On School ERP System software.",
-      ],
-      techStack: [
-        "HTML", "CSS", "React", "Flutter", "JavaScript", "Axios", "Material UI", "Node.js", "Express.js", "MongoDB", "Firebase"
-      ]
-    },
-    {
-      title: "Web Developer Intern",
-      company: "Radiant IT Services",
-      location: "Kharadi, Pune",
-      duration: "Sep 2023 - Dec 2023",
-      responsibilities: [
-        "Worked on UI designs, ensuring a seamless and attractive user interface.",
-        "Completed projects like QuantumCargo, a logistic management website.",
-      ],
-      techStack: [
-        "HTML", "CSS", "React", "JavaScript", "Axios", "Material UI"
-      ]
-    }
-  ];
-  
-  export default internshipData;
-  
+  {
+    title: "Junior Web Developer",
+    company: "Demand Media Bpm LLP",
+    location: "Magarpatta City, Pune",
+    duration: "27 Sep 2024 - Pursuing",
+    responsibilities: [
+      "Managing microsites for organizations, ensuring functionality, responsiveness, and optimal performance.",
+      "Creating landing pages (LPs) for various clients across different languages and countries to cater to localized needs.",
+      "Collaborating on custom software projects to deliver tailored solutions as per client requirements.",
+      "Optimizing React frontend for performance, improving page load times, and enhancing website functionality and user experience.",
+      "Communicating with cross-functional teams to gather requirements and deliver projects on time.",
+    ],
+    techStack: [
+      "HTML",
+      "CSS",
+      "React",
+      "JavaScript",
+      "Axios",
+      "Material UI",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Vercel",
+      "WordPress",
+    ],
+  },
+  {
+    title: "Software Developer Intern",
+    company: "Incronix Technology Pvt Ltd",
+    location: "Aundh, Pune",
+    duration: "July 2024 - Sep 2024",
+    responsibilities: [
+      "Optimized React frontend for performance, improving page load times, enhancing website functionality and user experience.",
+      "Currently Working On School ERP System software.",
+    ],
+    techStack: [
+      "HTML",
+      "CSS",
+      "React",
+      "Flutter",
+      "JavaScript",
+      "Axios",
+      "Material UI",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Firebase",
+    ],
+  },
+  {
+    title: "Web Developer Intern",
+    company: "Radiant IT Services",
+    location: "Kharadi, Pune",
+    duration: "Sep 2023 - Dec 2023",
+    responsibilities: [
+      "Worked on UI designs, ensuring a seamless and attractive user interface.",
+      "Completed projects like QuantumCargo, a logistic management website.",
+    ],
+    techStack: ["HTML", "CSS", "React", "JavaScript", "Axios", "Material UI"],
+  },
+];
+
+export default internshipData;
